3b3335f39c48313a458fd4458b9875f6414c5681
Jakub Jurczynski <JJURCZYNSKI@foss.pl>
2019-10-14 13:15:44 +0200

    split home component into smaller components and add styles

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss';
+import MenuList from '../../components/menu-list/MenuList'
+
+const Home = () => {
+    return (
+        <div className="homepage">
+            <MenuList />
+        </div>
+    );
+}
+
+export default Home;
