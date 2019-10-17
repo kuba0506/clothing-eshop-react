e7fd6fa573b7c18ad28d3d76f0ff03908b782e78
Jakub Jurczynski <JJURCZYNSKI@foss.pl>
2019-10-17 09:17:00 +0200

    update project with header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Shop from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 const App = () => {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/shop" component={Shop} />
-            </Switch>
-        </Router>
+        <Fragment>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/shop" component={Shop} />
+                </Switch>
+            </Router>
+        </Fragment>
     );
-}
+};
 
 export default App;
