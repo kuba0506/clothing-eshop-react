21dc7da13805d5bdb2588e599886d1ed867852c0
Jakub Jurczynski <JJURCZYNSKI@foss.pl>
2021-01-30 12:15:38 +0100

    Updates

---
+++ precache-manifest.a9cdcaa4f65ecfce231ab32437c6f1cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3105ea0deb1e25d2e0cfb3cbbd3e23fe",
+    "url": "/clothing-eshop-react/index.html"
+  },
+  {
+    "revision": "42f820939b33a5d81542",
+    "url": "/clothing-eshop-react/static/css/main.1f63ad4f.chunk.css"
+  },
+  {
+    "revision": "ccdf322ca369c4ed5c52",
+    "url": "/clothing-eshop-react/static/js/2.136e2acf.chunk.js"
+  },
+  {
+    "revision": "42f820939b33a5d81542",
+    "url": "/clothing-eshop-react/static/js/main.a307978d.chunk.js"
+  },
+  {
+    "revision": "6eefe246911852301129",
+    "url": "/clothing-eshop-react/static/js/runtime-main.13712b1c.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/clothing-eshop-react/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing-eshop-react/static/media/shopping-bag.30f07071.svg"
+  }
+]);
